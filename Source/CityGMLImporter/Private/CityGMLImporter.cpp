1dd7e76821788480d875da0a7f255c45d2a9aa46
Mirco <78418052+Mirwie@users.noreply.github.com>
2024-08-10 18:52:56 +0200

    Projektdateien hinzufügen.

---
+++ Source/CityGMLImporter/Private/CityGMLImporter.cpp
@@ -0,0 +1,80 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CityGMLImporter.h"
+#include "Misc/MessageDialog.h"
+#include "LevelEditor.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+#include "HAL/PlatformFilemanager.h"
+#include "Framework/Application/SlateApplication.h"
+#include "DesktopPlatformModule.h"
+
+static const FName CityGMLImporterTabName("CityGMLImporter");
+
+#define LOCTEXT_NAMESPACE "FCityGMLImporterModule"
+
+void FCityGMLImporterModule::StartupModule()
+{
+    // Dieser Code wird nach dem Laden des Moduls ausgeführt
+    FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+
+    TSharedPtr<FExtender> MenuExtender = MakeShareable(new FExtender());
+    MenuExtender->AddMenuExtension("FileProject", EExtensionHook::After, nullptr, FMenuExtensionDelegate::CreateRaw(this, &FCityGMLImporterModule::AddMenuExtension));
+
+    LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
+}
+
+void FCityGMLImporterModule::ShutdownModule()
+{
+    // Diese Funktion kann beim Herunterfahren aufgerufen werden, um das Modul aufzuräumen
+}
+
+void FCityGMLImporterModule::AddMenuExtension(FMenuBuilder& Builder)
+{
+    Builder.AddMenuEntry(
+        LOCTEXT("CityGMLParser", "Select CityGML-File"),
+        LOCTEXT("CityGMLParser_Tooltip", "Select a CityGML file to import"),
+        FSlateIcon(),
+        FUIAction(FExecuteAction::CreateRaw(this, &FCityGMLImporterModule::PluginButtonClicked))
+    );
+}
+
+void FCityGMLImporterModule::PluginButtonClicked()
+{
+   
+    TArray<FString> OutFiles;
+    IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get();
+    bool bOpened = false;
+
+    if (DesktopPlatform)
+    {
+        bOpened = DesktopPlatform->OpenFileDialog(  // Öffne den Datei-Dialog
+            nullptr,
+            TEXT("Select a CityGML file to import"),
+            FPaths::ProjectDir(),
+            TEXT(""),
+            TEXT("CityGML Files (*.xml)|*.xml"),
+            EFileDialogFlags::None,
+            OutFiles
+        );
+    }
+
+    if (bOpened && OutFiles.Num() > 0)
+    {
+        FString SelectedFile = OutFiles[0];
+        FText DialogText = FText::Format(
+            LOCTEXT("FileSelected", "Datei erfolgreich eingelesen: {0}"),
+            FText::FromString(SelectedFile)
+        );
+        FMessageDialog::Open(EAppMsgType::Ok, DialogText);
+    }
+    else
+    {
+        FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("FileNotSelected", "Keine Datei ausgewählt"));
+    }
+}
+
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCityGMLImporterModule, CityGMLImporter)
